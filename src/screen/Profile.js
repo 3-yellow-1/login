32d006f74e1e9151bb620fc227c5ef50aafc4683
3-yellow-1 <3.yelllow.1@gmail.com>
2023-07-14 16:45:09 +0900

    commit_230714_02

--- src/screen/Profile.js
+++ src/screen/Profile.js
@@ -15,10 +15,10 @@ const Profile = () => {
 
     // 로컬 스토리지에서 jtoken과 userid 가져오기
     const jtoken = localStorage.getItem('jtoken');
-    const userid = localStorage.getItem('userid');
+    const userid = localStorage.getItem('userid'); //브라우저의 로컬 저장소에서 'userid' 키와 연결된 값을 검색하는 데 사용되는 메서드 호출.
 
     // userid를 문자열로 변환하여 상태 변수에 저장
-    const parsedUserId = JSON.stringify(userid);
+    const parsedUserId = JSON.stringify(userid); //JSON.stringify()는 JavaScript 객체를 JSON 문자열 표현으로 변환하는 데 사용.
     setUserId(parsedUserId);
   
   }, [navigate]);
